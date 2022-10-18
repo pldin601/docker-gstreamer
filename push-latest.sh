c106f15290601c1aa260fa842b7d8ac2b2943b5c
roman <roman.lakhtadyr@gmail.com>
2022-10-18 11:17:22 +0100

    chore: add arch prefix to image tag

--- push-latest.sh
+++ push-latest.sh
@@ -2,21 +2,22 @@
 set -e
 
 DATE=$(date -u +"%Y-%m-%dT%H-%M-%SZ")
+ARCH=$(dpkg --print-architecture)
 
-echo $DATE
+echo $DATE $ARCH
 
 docker push restreamio/gstreamer:latest-dev-with-source
-docker tag restreamio/gstreamer:latest-dev-with-source restreamio/gstreamer:$DATE-dev-with-source
+docker tag restreamio/gstreamer:latest-dev-with-source restreamio/gstreamer:$DATE-dev-with-source-$ARCH
 docker push restreamio/gstreamer:$DATE-dev-with-source
 
 docker push restreamio/gstreamer:latest-dev
-docker tag restreamio/gstreamer:latest-dev restreamio/gstreamer:$DATE-dev
+docker tag restreamio/gstreamer:latest-dev restreamio/gstreamer:$DATE-dev-$ARCH
 docker push restreamio/gstreamer:$DATE-dev
 
 docker push restreamio/gstreamer:latest-prod
-docker tag restreamio/gstreamer:latest-prod restreamio/gstreamer:$DATE-prod
+docker tag restreamio/gstreamer:latest-prod restreamio/gstreamer:$DATE-prod-$ARCH
 docker push restreamio/gstreamer:$DATE-prod
 
 docker push restreamio/gstreamer:latest-prod-dbg
-docker tag restreamio/gstreamer:latest-prod-dbg restreamio/gstreamer:$DATE-prod-dbg
+docker tag restreamio/gstreamer:latest-prod-dbg restreamio/gstreamer:$DATE-prod-dbg-$ARCH
 docker push restreamio/gstreamer:$DATE-prod-dbg
